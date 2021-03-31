906499d7d0d0a6af151a264b1a1a0c841e7e3f9f
Bbeauprez <basile.beauprez.59@gmail.com>
2021-03-31 11:46:10 +0200

    rename project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esgi_project_1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mygesax.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- mygesax/wsgi.py
+++ mygesax/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for esgi_project_1 project.
+WSGI config for mygesax project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esgi_project_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mygesax.settings')
 
 application = get_wsgi_application()
