8021bd2a7c6b88c33eb98870ccaa714719c64ab2
Bbeauprez <basile.beauprez.59@gmail.com>
2021-03-31 18:54:23 +0200

    save to git

--- subject/views.py
+++ subject/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from subject.form import GradeCreationForm
+
+
+class AddGrade(generic.CreateView):
+    form_class = GradeCreationForm
+    success_url = reverse_lazy('/')
+    template_name = 'grades/add.html'
