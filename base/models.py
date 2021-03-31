f3e859967607dfcb4598db45941e38ab4776c949
Tom Délié <tomdelie78@gmail.com>
2021-03-31 17:29:37 +0200

    feat: add models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=30)
+    users = models.ManyToManyField(User)
+
+class Grade(models.Model):
+    note = models.PositiveIntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
