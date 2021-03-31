e2216de55261c56066898a11a2ffc0f10dc3ac71
Tom Délié <tomdelie78@gmail.com>
2021-03-31 16:39:25 +0200

    feat: init style

--- mygesax/settings.py
+++ mygesax/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
