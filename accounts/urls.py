ccd6fc8fc0a110230b28b8cb391fe6a7ff41e315
Tom Délié <tomdelie78@gmail.com>
2021-04-01 16:51:33 +0200

    feat: shared files system

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('export/', views.csv_export, name='csv_export'),
     path('import/', views.csv_import, name='csv_import'),
+    path('import-doc/', views.import_doc, name='import_doc'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,26 @@ from django.http import HttpResponse, JsonResponse
 import csv
 import json
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def import_doc(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(f"static/uploads/{myfile.name}", myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'home.html')
 
 def csv_export(request):
     response = HttpResponse(content_type='text/csv')
