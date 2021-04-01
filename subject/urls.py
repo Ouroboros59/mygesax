a4124bb67a8dd16fa9876823b02409efaf7baaff
Bbeauprez <basile.beauprez.59@gmail.com>
2021-04-01 18:20:01 +0200

    added add marks button

--- subject/urls.py
+++ subject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import AddGradeView, UserGradeView
 
 urlpatterns = [
-    path('add/grades/', AddGradeView.as_view(), name='add grade'),
+    path('add/grades/', AddGradeView.as_view(), name='add_grade'),
 ]
